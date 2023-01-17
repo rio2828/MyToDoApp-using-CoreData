6e5406c14e5102b39963e4095c32f2d84ef998e8
rio2828 <eddiekim1023@gmail.com>
2023-01-17 18:59:20 +0900

    파일 및 코어 데이터 생성

---
+++ MyToDoApp/ToDoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoData+CoreDataClass.swift
+//  MyToDoApp
+//
+//  Created by 김태현 on 2023/01/17.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoData)
+public class ToDoData: NSManagedObject {
+
+}
---
+++ MyToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToDoApp
+//
+//  Created by 김태현 on 2023/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
